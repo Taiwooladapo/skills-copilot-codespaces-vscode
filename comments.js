6162c16163082788d466a3017faecc0ac55852c0
Taiwo Oladapo <86772293+Taiwooladapo@users.noreply.github.com>
2023-07-21 21:16:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,20 +1,14 @@
 // create web server
-// 1. import http module
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const qs = require('querystring');
-const template = require('./lib/template.js');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-// 2. create web server object
-const app = http.createServer(function(request,response){
-    // 2.1. get url
-    const _url = request.url;
-    // 2.2. parse url
-    const queryData = url.parse(_url, true).query;
-    const pathname = url.parse(_url, true).pathname;
-    // 2.3. if pathname is root
-    if(pathname === '/'){
-        // 2.3.1. if queryData.id is undefined
-        if(queryData.id === undefined){
-            //
+// use body parser to get body from request
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// serve static files from public directory
+app.use(express.static('public'));
+
+// initialize comments
+var comments = []
